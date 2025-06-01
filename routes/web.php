553035ca70d9025a984cc31a51229df1e3b338c0
duyanh220 <tranduyanh220@gmail.com>
2025-06-02 06:54:48 +0700

    #3 - Tạo giao diện trang chủ

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,23 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardAdminController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/dashboardadmin', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
+
 
 require __DIR__.'/auth.php';
