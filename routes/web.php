f9289cf1188f8a8009265bcb7aab97cf36d636ab
nguyenquanghiep3404 <nguyenquanghiep3404@gmail.com>
2025-07-02 00:54:07 +0700

    #61 - thay doi mo hinh api

--- routes/web.php
+++ routes/web.php
@@ -115,24 +115,10 @@ Route::prefix('admin')
             // Routes với tham số {uploadedFile}
             Route::patch('/{uploadedFile}', [UploadedFileController::class, 'update'])->name('update');
             Route::delete('/{uploadedFile}', [UploadedFileController::class, 'destroy'])->name('destroy');
-
             Route::post('/{uploadedFile}/recrop', [UploadedFileController::class, 'recrop'])->name('recrop');
         });
         // Route quản lí vai trò
         Route::resource('roles', RoleController::class);
-
-        // 1. Route hiển thị trang chính của thư viện
-        Route::get('/media', [UploadedFileController::class, 'index'])->name('media.index');
-        // 2. Route xử lý việc tải file lên (sẽ được gọi bằng AJAX)
-        Route::post('/media', [UploadedFileController::class, 'store'])->name('media.store');
-        // 3. Route xử lý việc cập nhật thông tin file (sửa alt text, v.v. - AJAX)
-        Route::patch('/media/{uploadedFile}', [UploadedFileController::class, 'update'])->name('media.update');
-        Route::delete('/media/{uploadedFile}', [UploadedFileController::class, 'destroy'])->name('media.destroy');
-        Route::get('/media/fetch', [UploadedFileController::class, 'fetchForModal'])->name('media.fetchForModal');
-        Route::get('/media/trash', [UploadedFileController::class, 'trash'])->name('media.trash');
-        Route::post('/media/restore/{id}', [UploadedFileController::class, 'restore'])->name('media.restore');
-        Route::delete('/media/force-delete/{id}', [UploadedFileController::class, 'forceDelete'])->name('media.forceDelete');
-        Route::post('media/bulk-delete', [UploadedFileController::class, 'bulkDelete'])->name('media.bulk-delete');
 
         // Route::middleware('can:manage-content')->group(function () {
         Route::delete('products/gallery-images/{uploadedFile}', [ProductController::class, 'deleteGalleryImage'])
